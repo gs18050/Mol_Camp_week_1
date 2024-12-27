43887ec8a98479d2ce562a20c6dd28536760b1f2
gs18050 <dodream40910@gmail.com>
2024-12-27 12:06:09 +0900

    show grid images

--- app/src/main/java/com/example/tabapplication/ui/image/ImageFragment.kt
+++ app/src/main/java/com/example/tabapplication/ui/image/ImageFragment.kt
@@ -9,6 +9,30 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.tabapplication.databinding.FragmentImageBinding
 import com.example.tabapplication.R
 import android.widget.ImageView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class ImageAdapter(private val imageList: List<Int>) : RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
+    inner class ImageViewHolder(private val binding: FragmentImageBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(imageResId: Int) {
+            binding.imageView.setImageResource(imageResId)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val binding = FragmentImageBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ImageViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        holder.bind(imageList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return imageList.size
+    }
+}
 
 class ImageFragment : Fragment() {
 
@@ -29,11 +53,19 @@ class ImageFragment : Fragment() {
         _binding = FragmentImageBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val imageView: ImageView = binding.imageView
-        imageViewModel.setImage(R.drawable.testimg)
-        imageViewModel.imageResId.observe(viewLifecycleOwner) { resId ->
-            imageView.setImageResource(resId)
-        }
+        val imageResIds = listOf(
+            R.drawable.testimg,
+            R.drawable.testimg,
+            R.drawable.testimg,
+            R.drawable.testimg,
+            R.drawable.testimg
+        )
+
+        val imageAdapter = ImageAdapter(imageResIds)
+        val num_colum: Int=3
+        binding.imageRecyclerView.layoutManager = GridLayoutManager(requireContext(),num_colum)
+        binding.imageRecyclerView.adapter = imageAdapter
+
         return root
     }
 
